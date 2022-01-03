a00b7a5d426464cd3c7aa00d700e61bb71bca7bf
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-03 16:20:40 +0530

    try catch

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+
 const connect = ()=>{
     return mongoose.connect("mongodb://127.0.0.1:27017/Ecom_data")
 }
 
 const app = express();
+
+
 //product Schema
 const ProductSchema =new mongoose.Schema({
     prod_name :{type:String,required:true},
@@ -17,16 +20,31 @@ const ProductSchema =new mongoose.Schema({
     timestamps:true
 });
 
+
 const Product = mongoose.model("product",ProductSchema)
 
+
 app.post("products",async(res,req)=>{
-    const product = await Product.create(req.body);
+
+    try{
+        const product = await Product.create(req.body);
     return res.send(product);
+    }
+    catch(err){
+return res.statusCode(500).json({message: err.message});
+    }
+    
 })
 
 app.get("/products",async(req,res)=>{
-    const products = await Product.find().lean().exac();
-    return res.send(products);
+
+    try{
+        const products = await Product.find().lean().exac();
+        return res.send(products);
+    }
+    catch(err){
+        return res.statusCode(500).json({message: err.message});
+    }
 })
 
 
@@ -41,5 +59,6 @@ app.get("/products",async(req,res)=>{
 
 
 app.listen(2345,async()=>{
+    await connect()
     console.log("connecting to server 2345")
 })
